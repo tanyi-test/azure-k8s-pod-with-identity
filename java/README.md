e6264c23266e6bd8daafaa7763b6549b55f25e36
Tanyi Chen <tanchen@microsoft.com>
2020-12-23 13:15:41 +0800

    feat: add java sample

---
+++ java/README.md
@@ -0,0 +1,48 @@
+# Sample for using user managed identity in AKS
+
+### Prerequisites
+
+* [OracleJDK](https://www.oracle.com/java/technologies/javase-downloads.html) or [OpenJDK](https://openjdk.java.net/)
+* [Maven](https://maven.apache.org/)
+* [kubectl](https://kubernetes.io/docs/tasks/tools/install-kubectl/)
+
+### Run samples
+
+See [DefaultAzureCredential](https://github.com/Azure/azure-sdk-for-java/tree/master/sdk/identity/azure-identity#defaultazurecredential) and prepare the authentication works best for you.
+
+For more details on authentication, please refer to [AUTH.md](https://github.com/Azure/azure-sdk-for-java/blob/master/sdk/resourcemanager/docs/AUTH.md).
+
+```bash
+mvn clean compile exec:java # create identity and AKS, also write test-pod.yml and test.kubeconfig
+kubectl apply -f test-pod.yml --kubeconfig test.kubeconfig
+kubectl logs -f test
+``` 
+
+### Already has AKS with identity
+
+Replace `%s %s %s` in `test-pod.template.yml` with `subscription id`, `resource group name` and `identity client id`. Make sure the identity can create a virtual machine in the resource group.
+
+Add the following property in the AKS properties.
+```json
+{
+    "podIdentityProfile": {
+        "enabled": true,
+        "userAssignedIdentities": [{
+            "name": "default",
+            "namespace": "default",
+            "identity": {
+                "resourceId": "<identity id>",
+                "clientId": "<identity client id>",
+                "objectId": "<identity pricinpal id>"
+            }
+        }]
+    }
+}
+```
+
+Then run
+
+```bash
+kubectl apply -f test-pod.template.yml
+kubectl logs -f test
+```
---
+++ java/src/main/java/com/test/app/App.java
@@ -0,0 +1,118 @@
+package com.test.app;
+
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.authorization.models.BuiltInRole;
+import com.azure.resourcemanager.containerservice.fluent.models.ManagedClusterInner;
+import com.azure.resourcemanager.containerservice.models.AgentPoolMode;
+import com.azure.resourcemanager.containerservice.models.ContainerServiceNetworkProfile;
+import com.azure.resourcemanager.containerservice.models.ContainerServiceVMSizeTypes;
+import com.azure.resourcemanager.containerservice.models.CredentialResult;
+import com.azure.resourcemanager.containerservice.models.ManagedClusterAgentPoolProfile;
+import com.azure.resourcemanager.containerservice.models.ManagedClusterIdentity;
+import com.azure.resourcemanager.containerservice.models.ManagedClusterIdentityUserAssignedIdentities;
+import com.azure.resourcemanager.containerservice.models.ManagedClusterPodIdentity;
+import com.azure.resourcemanager.containerservice.models.ManagedClusterPodIdentityProfile;
+import com.azure.resourcemanager.containerservice.models.NetworkPlugin;
+import com.azure.resourcemanager.containerservice.models.OSType;
+import com.azure.resourcemanager.containerservice.models.ResourceIdentityType;
+import com.azure.resourcemanager.containerservice.models.UserAssignedIdentity;
+import com.azure.resourcemanager.msi.models.Identity;
+import com.azure.resourcemanager.resources.models.ResourceGroup;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.UUID;
+
+public class App {
+    public static String random(String prefix, int length) {
+        return prefix + UUID.randomUUID().toString().replace("-", "").substring(0, length - prefix.length());
+    }
+
+    public static void main(String[] args) throws IOException {
+        TokenCredential credential = new DefaultAzureCredentialBuilder().build();
+        AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+
+        AzureResourceManager azure = AzureResourceManager.authenticate(credential, profile).withDefaultSubscription();
+
+        String rgName = random("rg", 12);
+        String identityName = random("id", 12);
+        String aksName = random("aks", 12);
+        String dnsPrefix = random("dns", 12);
+        Region region = Region.US_EAST;
+
+        String resourceGroupForMsi = random("rg", 12);
+
+        ResourceGroup msiResourceGroup = azure.resourceGroups().define(resourceGroupForMsi)
+                .withRegion(region)
+                .create();
+
+        Identity identity = azure.identities().define(identityName)
+                .withRegion(region)
+                .withNewResourceGroup(rgName)
+                .withAccessTo(msiResourceGroup, BuiltInRole.CONTRIBUTOR)
+                .withAccessToCurrentResourceGroup(BuiltInRole.CONTRIBUTOR)
+                .create();
+
+        ManagedClusterInner managedCluster = new ManagedClusterInner()
+                .withAgentPoolProfiles(Collections.singletonList(
+                        new ManagedClusterAgentPoolProfile()
+                            .withName("nodepool1")
+                            .withVmSize(ContainerServiceVMSizeTypes.STANDARD_B2S)
+                            .withOsType(OSType.LINUX)
+                            .withMode(AgentPoolMode.SYSTEM)
+                            .withCount(1)
+                ))
+                .withDnsPrefix(dnsPrefix)
+                .withIdentity(
+                        new ManagedClusterIdentity()
+                            .withType(ResourceIdentityType.USER_ASSIGNED)
+                            .withUserAssignedIdentities(new HashMap<>(){{
+                                put(identity.id(), new ManagedClusterIdentityUserAssignedIdentities());
+                            }})
+                )
+                .withNetworkProfile(
+                        new ContainerServiceNetworkProfile()
+                            .withNetworkPlugin(NetworkPlugin.AZURE)
+                )
+                .withPodIdentityProfile(
+                        new ManagedClusterPodIdentityProfile()
+                            .withEnabled(true)
+                );
+        managedCluster.withLocation(region.toString());
+
+        managedCluster = azure.kubernetesClusters().manager().serviceClient().getManagedClusters().createOrUpdate(rgName, aksName, managedCluster);
+
+        managedCluster.withPodIdentityProfile(
+                managedCluster.podIdentityProfile()
+                    .withUserAssignedIdentities(Collections.singletonList(
+                        new ManagedClusterPodIdentity()
+                                .withName("default")
+                                .withNamespace("default")
+                                .withIdentity(new UserAssignedIdentity().withResourceId(identity.id()).withClientId(identity.clientId()).withObjectId(identity.principalId()))
+                    ))
+        );
+        managedCluster = azure.kubernetesClusters().manager().serviceClient().getManagedClusters().createOrUpdate(rgName, aksName, managedCluster);
+
+        String test_pod_template = new String(Files.readAllBytes(new File("test-pod.template.yml").toPath()), StandardCharsets.UTF_8);
+        String test_pod = String.format(test_pod_template, azure.subscriptionId(), resourceGroupForMsi, identity.clientId());
+        Files.write(new File("test-pod.yml").toPath(), test_pod.getBytes(StandardCharsets.UTF_8));
+
+        Optional<CredentialResult> credentialResult = azure.kubernetesClusters().listAdminKubeConfigContent(rgName, aksName).stream().findFirst();
+
+        if (credentialResult.isPresent()) {
+            Files.write(new File("test.kubeconfig").toPath(), credentialResult.get().value());
+        } else {
+            throw new RuntimeException("Cannot get admin config from kubernetes clusters");
+        }
+    }
+}
